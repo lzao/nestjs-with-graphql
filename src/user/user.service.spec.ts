7d4ecf14adc3f4c5d02867f677c13f55c0ed9f34
김준희 <lzqa@naver.com>
2021-05-14 12:15:58 +0900

    add createAccount test case

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,6 +4,8 @@ import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service"
 
+enum UserRole {Admin, Member};
+
 describe('UserService', () => {
     let service: UserService;
     let userRepository: UserRepository;
@@ -18,9 +20,21 @@ describe('UserService', () => {
     });
 
     describe("회원 생성", () => {
-        it("하나의 회원을 생성합니다.", async () => {
-            enum UserRole {Admin, Member};
+        it("회원을 생성할 때 이메일이 중복되면 에러를 발생합니다", async() => {
+            const createAccountInput: CreateAccountInput = {
+                email: 'lzao@naver.com',
+                password: '123',
+                nickname: 'lzao',
+                role: UserRole.Member
+            };
 
+            const createUser = User.of(createAccountInput);
+            const findUserRepositorySpy = jest.spyOn(userRepository, 'findOne')
+                .mockResolvedValue(createUser);
+            const result = await service.createAccount(createAccountInput);
+            expect(result).toEqual({ok:false, error: "이미 가입된 이메일 입니다."});
+        });
+        it("하나의 회원을 생성합니다.", async () => {
             const createAccountInput: CreateAccountInput = {
                 email: 'lzao@naver.com',
                 password: '123',
